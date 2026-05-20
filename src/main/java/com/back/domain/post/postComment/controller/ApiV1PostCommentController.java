54a069ec174fea2f62e234dec8378f99a9a6d385
jjun012 <duwns2057@naver.com>
2026-05-20 12:37:18 +0900

    014

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -4,6 +4,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +50,7 @@ public class ApiV1PostCommentController {
     }
     @GetMapping("/{id}/delete")
     @Transactional
-    public Map<String, Object> delete(
+    public RsData delete(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -59,10 +60,9 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        Map<String, Object> rsData = new LinkedHashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 댓글이 삭제되었습니다.".formatted(postComment.getId()));
-
-        return rsData;
+        return new RsData(
+                "200-1",
+                "%d번 댓글이 삭제되었습니다.".formatted(postComment.getId())
+        );
     }
 }
