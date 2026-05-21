323f80ff0e2d5795e260cf98b98eac5464775e4e
jjun012 <duwns2057@naver.com>
2026-05-21 11:35:51 +0900

    027-1

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -80,7 +80,7 @@ public class ApiV1PostController {
 
 
         return new RsData<>(
-                "200-1",
+                "201-1",
                 "%d번 글이 생성되었습니다.".formatted(post.getId()),
                 new PostWriteResBody(
                         totalCount, new PostDto(post)
