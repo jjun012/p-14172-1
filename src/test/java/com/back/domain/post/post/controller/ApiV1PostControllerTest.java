985c03bbffaa7fab16b77e55f00da6477e947370
jjun012 <duwns2057@naver.com>
2026-05-21 14:25:57 +0900

    033-2

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,4 +1,41 @@
 package com.back.domain.post.post.controller;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ApiV1PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("글 쓰기")
+    void t1() throws Exception {
+        // 회원가입 요청을 보냅니다.
+        ResultActions resultActions = mockMvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "제목",
+                                            "content": "내용"
+                                        }
+                                        """)
+                );
+    }
 }
