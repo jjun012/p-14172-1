a5a7d1ba15df9b405e969386c1040afb19a7af65
jjun012 <duwns2057@naver.com>
2026-05-21 12:21:01 +0900

    029

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,8 +1,9 @@
 package com.back.global.rsData;
 
 import com.back.domain.post.postComment.dto.PostCommentDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public record RsData<T>(String resultCode, int statusCode,String msg, T data) {
+public record RsData<T>(String resultCode, @JsonIgnore int statusCode, String msg, T data) {
     public RsData(String resultCode, String msg) {
         this(resultCode, msg, null);
     }
