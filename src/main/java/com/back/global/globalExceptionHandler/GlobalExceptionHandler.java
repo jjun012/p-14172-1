c84321c03603f3463e20f28e6cf0e4ac11e1b029
jjun012 <duwns2057@naver.com>
2026-05-26 12:19:49 +0900

    048-4

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.back.global.globalExceptionHandler;
 
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
@@ -9,15 +10,17 @@ import java.util.NoSuchElementException;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
-@RestControllerAdvice
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(NOT_FOUND)
-    public RsData<Void> handle() {
-        return new RsData<>(
-                "404-1",
-                "해당 데이터가 존재하지 않습니다."
+    public ResponseEntity<RsData<Void>> handle() {
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "404-1",
+                        "해당 데이터가 존재하지 않습니다."
+                ),
+                NOT_FOUND
         );
     }
 }
