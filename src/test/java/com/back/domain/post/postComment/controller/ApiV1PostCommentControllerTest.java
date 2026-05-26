a9b5888f7afdb113fb84437afd0547b809a82cdb
jjun012 <duwns2057@naver.com>
2026-05-26 15:09:10 +0900

    054-1

--- src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
+++ src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
@@ -1,4 +1,14 @@
 package com.back.domain.post.postComment.controller;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ApiV1PostCommentControllerTest {
+
 }
