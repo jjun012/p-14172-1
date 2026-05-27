7220a72ba62398caacd706d1b83382f6ea1c6826
jjun012 <duwns2057@naver.com>
2026-05-27 11:27:47 +0900

    065-1

--- src/main/java/com/back/domain/home/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/home/controller/HomeController.java
@@ -29,4 +29,20 @@ public class HomeController {
                 </div>
                 """.formatted(localHost.getHostName(), localHost.getHostAddress());
     }
+    @GetMapping(value = "/test/fetchPosts", produces = TEXT_HTML_VALUE)
+    @Operation(summary = "fetchPosts 테스트")
+    public String testFetchPosts() {
+        return """
+                <script>
+                console.clear();
+                
+                fetch("http://localhost:8080/api/v1/posts")
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                    console.log(data[0].title);
+                  });
+                </script>
+                """;
+    }
 }
